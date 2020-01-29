61e63be16e82a987f7a489e201b56d30bd613fd7
Kevin Lee <lee423@purdue.edu>
2020-01-29 14:40:09 -0500

    aaa

--- Exercise_3.2.py
+++ Exercise_3.2.py
@@ -1,4 +1,15 @@
-def do_twice(f):
-    f()
-    f()
+def do_twice(f, num):
+    f(num)
+    f(num)
 
+
+def print_spam():
+    print('spam')
+
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+#do_twice(print_spam)
+
+do_twice(print_twice, 'spam')
