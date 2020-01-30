1e734ce90f53369428371e54426322e35869a7eb
Kevin Lee <lee423@purdue.edu>
2020-01-29 20:42:16 -0500

    asda

--- Exercise_3.2.py
+++ Exercise_3.2.py
@@ -1,15 +1,20 @@
+#Kevin Lee
+#assign 1-1
+
+#example from textbook
 def do_twice(f, num):
     f(num)
     f(num)
 
-
+#example from textbook
 def print_spam():
     print('spam')
 
+#print twice question
 def print_twice(bruce):
     print(bruce)
     print(bruce)
 
-#do_twice(print_spam)
-
+#input string, and put it into pring_twice function
+#as result, total output 4.
 do_twice(print_twice, 'spam')
