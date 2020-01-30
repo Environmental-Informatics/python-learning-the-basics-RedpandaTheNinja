a3cf239b70c4b12512350d8374e618035714ed7e
Kevin Lee <lee423@purdue.edu>
2020-01-29 20:39:21 -0500

    asda

--- Exercise_3.3.py
+++ Exercise_3.3.py
@@ -1,13 +1,20 @@
+#Kevin Lee
+#Assin 1
 
+#examples from text  book
 # print('+','-','-','-','-','+','-','-','-','-','+')
 # print('+', end=' ')
 # print('-')
 # print('|', ' ', ' ', ' ',' ', '|', ' ', ' ', ' ',' ' ,'|')
 
-
+#creating 2x2
+#first create each side by string of charc
 string1 = "+----+----+"
 string2 ="|    |    |"
 # print(string1)
+
+#create a function 2 different aruments as inputs
+#cprint as much as problem asks to
 def easy(arg1, arg2):
     print(arg1)
     print(arg2)
@@ -23,6 +30,8 @@ def easy(arg1, arg2):
 
 easy(string1,string2)
 
+#double the length from previous stringg
+#since it is being called later, overwirte can be performed
 string1 = "+----+----+----+----+"
 string2 ="|    |    |    |    |"
 def easy2(arg1, arg2):
