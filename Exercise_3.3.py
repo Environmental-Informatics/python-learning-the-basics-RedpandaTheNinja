59bc54f7a99a14841b94a3ff105e1ea747f855fe
Kevin Lee <lee423@purdue.edu>
2020-01-29 20:28:49 -0500

    asda

--- Exercise_3.3.py
+++ Exercise_3.3.py
@@ -1,12 +1,23 @@
 
-print('+','-','-','-','-','+','-','-','-','-','+')
+# print('+','-','-','-','-','+','-','-','-','-','+')
 # print('+', end=' ')
 # print('-')
-print('|', ' ', ' ', ' ',' ', '|', ' ', ' ', ' ',' ' ,'|')
-print('=======================')
-
+# print('|', ' ', ' ', ' ',' ', '|', ' ', ' ', ' ',' ' ,'|')
+# print('=======================')
+string1 = "+----+----+"
+string2 ="|    |    |"
+# print(string1)
 def easy(arg1, arg2):
     print(arg1)
     print(arg2)
+    print( arg2 )
+    print( arg2 )
+    print( arg2 )
+    print( arg1 )
+    print(arg2)
+    print( arg2 )
+    print( arg2 )
+    print( arg2 )
+    print( arg1 )
 
-easy
+easy(string1,string2)
